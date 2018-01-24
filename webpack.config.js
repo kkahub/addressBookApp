32e01cf045c322030d5864c886a8b0e1618a1255
kka <oceco@naver.com>
2018-01-24 18:10:35 +0900

    package.json add

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: __dirname + '/public/',
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        inline: true,
+        port: 7777,
+        contentBase: __dirname + '/public/'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ['react-hot-loader/webpack','babel-loader?' + JSON.stringify({
+                    cacheDirectory: true,
+                    presets: ['es2015', 'react']
+                })],
+                exclude: /node_modules/,
+            }
+        ]
+    }
+};
